f9a7a5226493aa56bbf577e46e86f008f13bc6a7
GabrielaAntolinez <36423718+GabrielaAntolinez@users.noreply.github.com>
2018-04-23 00:24:15 -0500

    Update laberinto.py

--- laberinto.py
+++ laberinto.py
@@ -9,24 +9,47 @@ def buscarX(lista):
             if "x" in str(laberinto[x][y]) :
                 return [x,y]
 
+    
 def buscarCeros(laberinto,listax):
     if ("x" in laberinto[listax[0]][listax[1]] or "0" in laberinto[listax[0]][listax[1]]):
-        #if laberinto[listax[0]-1][listax[1]]=="0":
-            #print ("arrib")
-            #return buscarCeros(laberinto,(listax[0]-1,listax[1])) 
+        if laberinto[listax[0]][listax[1]+1]=="Y" or laberinto[listax[0]][listax[1]-1]=="Y" or laberinto[listax[0]+1][listax[1]]=="Y" or laberinto[listax[0]-1][listax[1]]=="Y":
+            print("Se puede solucionar el Laberinto")
+        if laberinto[listax[0]-1][listax[1]]=="0":
+            if([(listax[0]-1,listax[1])] in listarep):
+                print (" ")
+            else:
+                print ("arrib")
+                listarep.append([(listax[0]-1,listax[1])])
+                print(listarep)
+                return buscarCeros(laberinto,(listax[0]-1,listax[1]))
+            
         if laberinto[listax[0]+1][listax[1]]=="0":
-            print ("abaj")
-            listarep.append([listax])
-            print(listarep)
-            return buscarCeros(laberinto,(listax[0]+1,listax[1])) 
-        ##if laberinto[listax[0]][listax[1]-1]=="0":
-            #print ("izq")
-            #return buscarCeros(laberinto,(listax[0],listax[1]-1))
+            if([(listax[0]+1,listax[1])] in listarep):
+                print (" ")
+            else:
+                print ("abaj")
+                listarep.append([(listax[0]+1,listax[1])])
+                print(listarep)
+                return buscarCeros(laberinto,(listax[0]+1,listax[1]))
+            
+        if laberinto[listax[0]][listax[1]-1]=="0":
+            if([(listax[0],listax[1]-1)] in listarep):
+                print (" ")
+            else:
+                print ("izq")
+                listarep.append([(listax[0],listax[1]-1)])
+                print(listarep)
+                return buscarCeros(laberinto,(listax[0],listax[1]-1))
+            
         if laberinto[listax[0]][listax[1]+1]=="0":
-            print ("dere")
-            listarep.append([listax])
-            print(listarep)
-            return buscarCeros(laberinto,(listax[0],listax[1]+1))
+            if([(listax[0],listax[1]+1)] in listarep):
+                print (" ")
+            else:
+                print ("dere")
+                listarep.append([(listax[0],listax[1]+1)])
+                print(listarep)
+                return buscarCeros(laberinto,(listax[0],listax[1]+1))
+       
     
 
 
@@ -34,5 +57,6 @@ salida = []
 with open('laberinto.txt', 'r') as f:
     laberinto = [linea.split() for linea in f]
 
+listarep=[buscarX(laberinto) ]
 print(buscarX(laberinto))
 buscarCeros(laberinto,buscarX(laberinto))
